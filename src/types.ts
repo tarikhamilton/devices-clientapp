a8fdab5ef82da2286fdeb343db0191bf7a6e9fdb
Tarik Hamilton <me@tarikhamilton.com>
2021-08-30 21:24:44 -0400

    Add styling libraries and remove CRA boilerplate

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Device {
+  id: string
+  /** System name. */
+  name: string
+  type: string
+  /** HDD capacity. */
+  capacity: number
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        'ninja-red': '#ce2043',
+        'ninja-gray': '#384852',
+        'ninja-blue': '##25498d',
+        'ninja-lightblue': '#eef3fa',
+      },
+    },
+  },
+}
